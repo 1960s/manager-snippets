12a4dada595e26a760e4fb07d58ac07c3ff9e4c4
ernestpworrell <36313089+1960s@users.noreply.github.com>
2023-03-08 14:54:32 -0600

    added new folder

---
+++ scripts/snippets-init.js
@@ -0,0 +1,7 @@
+
+
+
+
+
+document.querySelector('.card').click() //initial card, won't work until after snippetFields definition cards script find out why
+document.getElementById("main").click() 
---
+++ scripts/snippets-sidebar.js
@@ -0,0 +1,71 @@
+//SIDEBAR HTML GENERATION
+  function setHTMLSidebar(){
+    sidebarObj = {}
+    let sidebarValues = []
+    let sidebarHTML = []
+    let sidebarKeys = ["folders", "lang", "tags", "author", "type",]
+    //total values of group types, eg all tags
+    sidebarKeys.forEach(el => sidebarValues.push(totalVal(el)))
+    //parallel to keys
+    sidebarKeys.forEach((el, i) => sidebarHTML.push(sidebarGroupHTML(el, sidebarValues[i])))
+    
+    
+    let allSnippets = `  
+<div class="cardGroupTitle">Main</div>
+<div class="sidebarBoxes" >
+<div id="main">All Snippets</div>
+  </div>
+`
+    document.getElementById(`sidebarBox`).innerHTML = allSnippets + sidebarHTML.join("")
+  }
+
+
+
+
+  //CREATES ARRAY OF TOTAL VALUES IN OBJECT KEYS, show input and show output examples
+  function totalVal(objKey){
+    let valArr = []
+    for (const snippet of snippets){
+      snippet[objKey].forEach((el) =>{
+        if (!valArr.includes(el)){valArr.push(el)}
+      })
+    }
+    return valArr
+  }
+
+
+  //change function name to verb
+  function sidebarGroupHTML(objKey, objVal){
+  
+    let idCounter = 0 // must remain top level in function, can remove
+    let sidebarGroupValues = []
+    for (const value of objVal){
+    
+      let sidebarVal = []
+      for (const snippet of snippets){
+        if(snippet[objKey].includes(value)){
+          sidebarVal.push(snippet.cardID)
+        }
+      }
+
+      //WIRING sidebarObj up to snippets Obj
+      sidebarObj[`${objKey}${idCounter}`] = sidebarVal
+      
+      
+      let valueHTML = `
+<div id=${objKey}${idCounter}>
+${value}  ${sidebarVal.length}
+  </div>
+`      
+      sidebarGroupValues.push(valueHTML)
+      idCounter++
+    }
+
+    // HTML SIDEBAR CONTENT    
+    return `
+<div class="cardGroupTitle">${objKey}</div>
+<div class="sidebarBoxes" id="${objKey}Group">
+${sidebarGroupValues.join("")} 
+  </div>
+`
+  }
